c7334d29aa171cdff4d26d79b079c2efc6127514
Jens W. Klein <jk@kleinundpartner.at>
2019-12-20 12:54:55 +0100

    now with hub

--- .screenlayout/office.sh
+++ .screenlayout/office.sh
@@ -1,7 +1,7 @@
-O#!/bin/sh
+#!/bin/sh
 xrandr \
-    --output HDMI-2 --scale 1.4x1.4               --mode 3840x2160                 --rotate normal  \
-    --output eDP-1  --scale 1.333x1.333 --primary --mode 2560x1440 --pos 5376x1104 --rotate normal  \
     --output DP-1   --off \
-    --output DP-2   --off \
     --output HDMI-1 --off \
+    --output HDMI-2 --off \
+    --output DP-2   --scale 1.4x1.4               --mode 3840x2160                 --rotate normal  \
+    --output eDP-1  --scale 1.333x1.333 --primary --mode 2560x1440 --pos 5376x1104 --rotate normal  \
